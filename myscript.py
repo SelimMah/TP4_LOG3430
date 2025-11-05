3b3140528bfecdde4720d41e6724b4d77c9c4999
Omar-Zed <54953160+Omar-Zed@users.noreply.github.com>
2025-11-05 11:13:48 -0500

    Fixed commit values

--- myscript.py
+++ myscript.py
@@ -1,8 +1,8 @@
 import os
 
 # Define the good and bad commits
-goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
-badhash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c "
+badhash = "1d8748281263e8e7efe7b85c828cd3f600d96bfc "
 
 # Start git bisect
 os.system(f"git bisect start {badhash} {goodhash}")
